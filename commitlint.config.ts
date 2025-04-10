6f6c2eba96fcbf8db45a02da5561757fa263a5b1
sushikansai <michaelsagun013@gmail.com>
2025-04-10 13:02:36 +0800

    wip: husky add

---
+++ commitlint.config.ts
@@ -0,0 +1,19 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [2, 'never', ['upper-case']],
+
+    'subject-empty': [0],
+
+    'type-enum': [
+      2,
+      'always',
+      ['feature', 'fix', 'adjust', 'style', 'refactor', 'wip', 'test', 'add'],
+    ],
+
+    'header-max-length': [2, 'always', 72],
+
+    'type-case': [0],
+    'scope-enum': [0],
+  },
+};
