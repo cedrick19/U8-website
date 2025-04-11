3e9f043c0e2570e2deb68ee83f376f46ca016e99
sushikansai <michaelsagun013@gmail.com>
2025-04-11 18:10:28 +0800

    add SegmentedTabs.tsx reusable component

---
+++ src/components/SegmentedTabs/SegmentedTabs.tsx
@@ -0,0 +1,50 @@
+import { cn } from '@/globals/utils';
+import { Button, Segmented } from 'framework7-react';
+import { useState } from 'react';
+
+interface SegmentButtons {
+  tabName: string;
+  tabLink: string;
+  component?: React.ReactNode;
+}
+
+interface SegmentedTabsProps {
+  tabArray: SegmentButtons[];
+  defaultActiveTabIndex?: number;
+  className?: string;
+}
+
+export const SegmentedTabs: React.FC<SegmentedTabsProps> = ({
+  tabArray,
+  defaultActiveTabIndex = 0,
+  className,
+}) => {
+  const [activeTab, setActiveTab] = useState<number>(defaultActiveTabIndex);
+
+  return (
+    <Segmented
+      strong
+      round
+      className={cn('h-12 w-full items-center border border-black bg-white', className)}
+    >
+      {tabArray.map((tab, ids) => (
+        <Button
+          key={ids}
+          tabLink={`#${tab.tabLink}`}
+          round
+          active={activeTab === ids}
+          onClick={() => setActiveTab(ids)}
+        >
+          <span
+            className={cn(
+              'whitespace-pre-wrap text-xs',
+              activeTab === ids ? 'text-white' : 'text-gradient',
+            )}
+          >
+            {tab.tabName}
+          </span>
+        </Button>
+      ))}
+    </Segmented>
+  );
+};
