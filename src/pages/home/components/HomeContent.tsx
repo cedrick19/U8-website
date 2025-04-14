3f48ab3970ed156ba8b1a4903cebf77f2b51fc31
cedy123 <cedrickgueta056@gmail.com>
2025-04-14 13:55:06 +0800

    homecontent update

--- src/pages/home/components/HomeContent.tsx
+++ src/pages/home/components/HomeContent.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, Button } from 'framework7-react';
 import Video from '@/assets/image/homebody/live.png';
-import Lottery from '@/assets/image/homebody/lottery.png';
-import Electronic from '@/assets/image/homebody/electronic.png';
+import Lottery from '@/assets/image/homebody/lottery.svg';
+import Electronic from '@/assets/image/homebody/electronic.svg';
 import Sports from '@/assets/image/homebody/sports.png';
 import Fishing from '@/assets/image/homebody/fishing.png';
 import Poker from '@/assets/image/homebody/poker.png';
@@ -14,101 +14,93 @@ const HomeBody = () => {
       id: 1,
       name: 'Sports',
       image: Sports,
-      color: 'bg-purple-800',
+      color: 'bg-[#3a1e7a]',
     },
     {
       id: 2,
       name: 'Fishing',
       image: Fishing,
-      color: 'bg-purple-800',
+      color: 'bg-[#3a1e7a]',
     },
     {
       id: 3,
       name: 'Poker',
       image: Poker,
-      color: 'bg-purple-800',
+      color: 'bg-[#3a1e7a]',
     },
     {
       id: 4,
       name: 'Chain',
       image: Chain,
-      color: 'bg-purple-800',
+      color: 'bg-[#3a1e7a]',
     },
   ];
 
   return (
     <div className="flex flex-col pb-20">
-      <div className="grid grid-cols-2 gap-3">
-        <Card className="row-span-2 flex h-[290px] w-full flex-col overflow-hidden rounded-xl border-b-4 border-b-white border-t-white bg-[#4C2CCA4D]/10">
+      <div className="grid grid-cols-2 gap-4 p-4">
+        <Card className="m-0 overflow-hidden rounded-xl border-2 border-x-white border-y-purple-50 bg-[#4C2CCA4D]/10">
           <LineCardpolygon />
-          <div className="relative h-full w-full overflow-hidden">
-            <img
-              src={Video}
-              alt="Live Video"
-              className="absolute left-0 top-0 h-full w-full object-contain"
-            />
-          </div>
-          <CardContent className="flex flex-col items-center justify-center bg-transparent text-center">
-            <h2 className="text-gradient text-center text-3xl font-extrabold md:text-2xl">
+          <div className="flex h-full flex-col items-center p-4">
+            <div className="relative mb-5 h-32 w-full">
+              <img src={Video} className="left-0 top-0 h-full w-full object-contain" />
+            </div>
+            <h2 className="mt-auto text-center text-3xl font-extrabold text-purple-800">
               LIVE VIDEO
             </h2>
-          </CardContent>
+          </div>
+        </Card>
+        <Card className="m-0 overflow-hidden rounded-xl border-2 border-x-white border-y-purple-50 bg-[#4C2CCA4D]/10">
+          <LineCardpolygon />
+          <div className="flex h-full flex-col items-center p-4">
+            <div className="relative mb-5 h-32 w-full">
+              <img src={Lottery} className="left-0 top-0 h-full w-full object-contain" />
+            </div>
+            <h2 className="mt-auto text-center text-3xl font-extrabold text-purple-800">
+              LOTTERY TICKET
+            </h2>
+          </div>
         </Card>
 
-        <div className="flex flex-col">
-          <Card className="relative mb-0 flex h-[142px] items-start overflow-hidden rounded-xl border-y border-white bg-purple-800/5 px-4 py-2">
-            <LineCardpolygon />
-            <CardContent className="z-10 flex flex-col justify-center gap-2 p-0">
-              <h2 className="text-gradient text-xl font-extrabold text-purple-800">
-                LOTTERY TICKET
-              </h2>
-            </CardContent>
-
-            <img
-              src={Lottery}
-              alt="Colorful Lottery Ticket"
-              className="absolute bottom-2 right-1 z-0 h-20 w-20 object-contain opacity-90"
-            />
-          </Card>
-
-          <Card className="relative flex h-[118px] items-start overflow-hidden rounded-xl border-b-white border-t-white bg-[#4C2CCA4D]/10 px-2 pt-2">
-            <LineCardpolygon />
-            <CardContent className="z-10 flex flex-1 flex-col justify-center space-y-1 p-0">
-              <h2 className="text-gradient text-xl font-extrabold text-purple-800">
-                ELECTRONIC GAMES
-              </h2>
-            </CardContent>
-
-            <img
-              src={Electronic}
-              alt="Lottery Diamond"
-              className="absolute bottom-2 right-1 z-0 h-20 w-20 object-contain opacity-90"
-            />
-          </Card>
-        </div>
+        <Card className="col-span-2 m-0 overflow-hidden rounded-xl border-2 border-x-white border-y-purple-50 bg-[#4C2CCA4D]/10">
+          <LineCardpolygon />
+          <div className="flex flex-row items-center justify-center p-4 text-center">
+            <div className="flex-1">
+              <h2 className="relative text-3xl font-extrabold text-purple-900">ELECTRONIC GAMES</h2>
+            </div>
+            <div className="flex-1">
+              <img
+                src={Electronic}
+                className="relative mx-auto w-full max-w-[200px] object-contain"
+              />
+            </div>
+          </div>
+        </Card>
       </div>
 
-      <div className="grid grid-cols-4 gap-4 px-4">
+      <div className="grid grid-cols-4 gap-x-2 px-4">
         {categories.map((category) => (
           <Card
             key={category.id}
-            className="m-0 overflow-hidden rounded-xl bg-purple-100 shadow-md"
+            className="m-0 h-24 overflow-hidden rounded-xl border-2 border-white bg-purple-100 p-1 pt-2 shadow-md"
           >
             <LineCardpolygon />
             <CardContent className="flex flex-col items-center justify-between p-0">
-              <div className="flex h-14 w-full items-center justify-center p-4">
+              <div className="flex h-10 w-auto items-center justify-center p-2">
                 <img
                   src={category.image || '/placeholder.svg'}
                   alt={category.name}
-                  className="h-full w-full object-cover"
+                  className="left-0 top-0 h-auto w-auto object-contain"
                 />
               </div>
               <div className="w-full p-1 pb-4">
-                <Button
-                  className={`${category.color} w-full rounded-xl px-2 font-medium text-white`}
-                >
-                  {category.name}
-                </Button>
+                <div className="h-8 rounded-xl border-4 border-[#3a1e7a]">
+                  <Button
+                    className={`${category.color} h-5 w-full px-2 text-xs font-bold text-white`}
+                  >
+                    {category.name}
+                  </Button>
+                </div>
               </div>
             </CardContent>
           </Card>
