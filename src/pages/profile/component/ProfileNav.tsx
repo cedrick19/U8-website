f19bb0f133e6327accfd5ea1c02bc6fa91129313
virgil-ferolino <gitMaleFerolino@gmail.com>
2025-04-15 13:29:29 +0800

    restructure: folder directory, added advance security verification

--- src/pages/profile/component/ProfileNav.tsx
+++ src/pages/profile/component/ProfileNav.tsx
@@ -1,4 +1,4 @@
-import { Navbar, NavLeft, Link, f7, Icon } from 'framework7-react';
+import { Navbar, Link, f7, Icon } from 'framework7-react';
 
 interface ProfileNavigation {
   title: string;
@@ -6,13 +6,11 @@ interface ProfileNavigation {
 
 const ProfileNav = ({ title }: ProfileNavigation) => {
   return (
-    <Navbar innerClassName="bg-white relative">
-      <NavLeft>
-        <Link onClick={() => f7.views.main.router.back()}>
-          <Icon material="chevron_left" size={35} className="text-gradient" />
-        </Link>
-      </NavLeft>
-      <p className="text-gradient absolute w-full text-center text-xl font-bold">{title}</p>
+    <Navbar innerClassName="bg-white w-full relative">
+      <Link onClick={() => f7.views.main.router.back()}>
+        <Icon material="chevron_left" size={35} className="text-gradient" />
+      </Link>
+      <p className="text-gradient absolute w-[95%] text-center text-xl font-bold">{title}</p>
     </Navbar>
   );
 };
--- src/pages/profile/settings/Settings.tsx
+++ src/pages/profile/settings/Settings.tsx
@@ -1,8 +1,8 @@
 import { Block, Button, f7, Page } from 'framework7-react';
-import SettingItem from '../component/SettingItem';
+import SettingItem from '@/pages/profile/component/SettingItem';
 import { useState } from 'react';
-import ProfileNav from '../component/ProfileNav';
-import CustomPopUp from '../component/CustomPopUp';
+import ProfileNav from '@/pages/profile/component/ProfileNav';
+import CustomPopUp from '@/pages/profile/component/CustomPopUp';
 
 const Settings = () => {
   const [loginOpen, setLoginOpen] = useState(false);
