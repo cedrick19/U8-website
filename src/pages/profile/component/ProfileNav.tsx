91b72b045c733de8f891ee5b7a00e2ea63c871b6
cedy123 <cedrickgueta056@gmail.com>
2025-04-16 17:47:20 +0800

    add reusable navright in profilenav

--- src/pages/profile/component/ProfileNav.tsx
+++ src/pages/profile/component/ProfileNav.tsx
@@ -1,23 +1,19 @@
 import { cn } from '@/globals/utils';
-import { Navbar, Link, f7, Icon } from 'framework7-react';
+import { Navbar, Link, f7, Icon, NavRight } from 'framework7-react';
 
 interface ProfileNavigation {
   title: string;
+  navRight?: React.ReactNode;
 }
 
-const ProfileNav = ({ title }: ProfileNavigation) => {
+const ProfileNav = ({ title, navRight }: ProfileNavigation) => {
   return (
     <Navbar innerClassName="bg-white w-full relative">
-      <Link onClick={() => f7.views.main.router.back()}>
+      <Link aria-label="Go back" onClick={() => f7.views.main.router.back()}>
         <Icon material="chevron_left" size={35} className="text-gradient" />
       </Link>
-      <p
-        className={cn(
-          'text-gradient absolute w-[95%] text-center text-xl font-bold',
-        )}
-      >
-        {title}
-      </p>
+      <p className={cn('text-gradient absolute w-[95%] text-center text-xl font-bold')}>{title}</p>
+      {navRight && <NavRight className="gap-2 pr-2">{navRight}</NavRight>}
     </Navbar>
   );
 };
--- src/pages/profile/refresh/Refresh.tsx
+++ src/pages/profile/refresh/Refresh.tsx
@@ -7,7 +7,10 @@ const Refresh = () => {
   return (
     <Page name="Refresh" className="relative">
       <div className="absolute h-[40%] w-full rounded-br-full bg-gradient-to-br from-[#381E7B]/20 via-transparent to-transparent backdrop:blur-3xl" />
-      <ProfileNav title="Refresh" />
+      <ProfileNav
+        title="Refresh"
+        navRight={<p className="text-base text-purple-800">Refill Record</p>}
+      />
       <Block>
         <SegmentedTabs tabArray={RefreshOptions} />
       </Block>
