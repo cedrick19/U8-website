36c52ab55172c28400049a862e06621746e5b1eb
cedy123 <cedrickgueta056@gmail.com>
2025-04-16 13:53:08 +0800

    create refresh and huawang tab

---
+++ src/pages/profile/refresh/Refresh.tsx
@@ -0,0 +1,18 @@
+import { Block, Page } from 'framework7-react';
+import ProfileNav from '@/pages/profile/component/ProfileNav';
+import { SegmentedTabs } from '@/components/SegmentedTabs';
+import { RefreshOptions } from '@/pages/profile/refresh/components/utils';
+
+const Refresh = () => {
+  return (
+    <Page name="Refresh" className="relative">
+      <div className="absolute h-[40%] w-full rounded-br-full bg-gradient-to-br from-[#381E7B]/20 via-transparent to-transparent backdrop:blur-3xl" />
+      <ProfileNav title="Refresh" />
+      <Block>
+        <SegmentedTabs tabArray={RefreshOptions} />
+      </Block>
+    </Page>
+  );
+};
+
+export default Refresh;
