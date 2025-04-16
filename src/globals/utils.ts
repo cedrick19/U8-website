9d48ff6e328e799e91645906a57350eae6690a6d
virgil-ferolino <gitMaleFerolino@gmail.com>
2025-04-16 10:27:26 +0800

    add: global function for onClick navigate

--- src/globals/utils.ts
+++ src/globals/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { f7 } from 'framework7-react';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const clickNavigate = (id: string, url: string) => {
+  document.getElementById(id)?.blur();
+  f7.view.main.router.navigate(url);
+};
