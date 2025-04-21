bc39e6fe677d5de0329232b7ce280cfba31f7ddd
cedy123 <cedrickgueta056@gmail.com>
2025-04-21 18:30:20 +0800

    fix fonts layouts and remove svg's

--- src/pages/profile/refresh/components/HuangwangPage.tsx
+++ src/pages/profile/refresh/components/HuangwangPage.tsx
@@ -1,6 +1,4 @@
 import { List, Icon, Button, Block, Input } from 'framework7-react';
-import Question from '@/assets/image/icons/question.svg';
-import Warning from '@/assets/image/icons/warning.svg';
 import { cn } from '@/globals/utils';
 
 const balanceSummary = [
@@ -9,13 +7,14 @@ const balanceSummary = [
   { label: 'Expected', value: '0.00' },
   { label: 'Frozen Amount', value: '0.00' },
 ];
+
 export const HuangWangPage = () => {
   return (
     <>
       <Block className="my-3 rounded-3xl bg-white p-4 shadow-sm">
         <div className="flex space-x-2">
           <h2 className="text-base font-bold text-purple-900">Withdraw the HSBC account</h2>
-          <img src={Question} alt="Help icon" />
+          <span className="material-icons mr-1 mt-1 text-[18px] text-gray-500">help</span>
         </div>
         <div className="flex flex-col gap-3">
           <List className="m-0">
@@ -37,14 +36,17 @@ export const HuangWangPage = () => {
           </List>
 
           <div className="flex items-start">
-            <img src={Warning} alt="Warning" className="h-5 w-5" />
-            <p className="px-2 text-sm text-red-500">
+            <span className="material-icons relative -top-[1px] mr-1 text-[18px] text-red-500">
+              warning
+            </span>
+            <p className="text-sm text-red-500">
               Please carefully check the HSBC account. The wrong account funds will not be
               available.
             </p>
           </div>
         </div>
       </Block>
+
       <Block className="my-3 rounded-3xl bg-white p-5 shadow-sm">
         <h2 className="text-lg font-bold text-purple-900">Cash withdrawal</h2>
 
@@ -63,13 +65,14 @@ export const HuangWangPage = () => {
             Max
           </Button>
         </div>
+
         <div className="mb-4 rounded-lg bg-purple-50 p-3 text-sm text-black">
           {balanceSummary.map(({ label, value }) => (
             <div key={label} className="mb-1 flex items-center justify-between">
               <span className="text-purple-900label flex items-center font-semibold text-purple-800">
                 {label}
                 {label === 'Frozen Amount' && (
-                  <img src={Question} alt="info" className="ml-1 h-4 w-4" />
+                  <span className="material-icons ml-1 text-[16px] text-gray-500">help</span>
                 )}
               </span>
               <span className="font-bold">{value}</span>
@@ -77,17 +80,15 @@ export const HuangWangPage = () => {
           ))}
         </div>
 
-        <p className="text-xs text-gray-600">
-          Contact for first presentation @U8hdkefu8 Official grid customer service and let the
-          customer service retain your aircraft number. The customer service will be checked twice
-          to ensure the safety of fund.
+        <p className="text-xs font-medium text-black">
+          Contact for first presentation
+          <span className="font-bold text-purple-900"> @U8hdkefu8 </span>
+          Official grid customer service and let the customer service retain your aircraft number.
+          The customer service will be checked twice to ensure the safety of fund.
         </p>
       </Block>
-      <Button
-        className={cn(
-          'h-14 w-full rounded-lg bg-purple-900 py-4 font-medium normal-case text-white',
-        )}
-      >
+
+      <Button className="h-14 w-full rounded-lg bg-purple-900 py-4 font-medium normal-case text-white">
         Immediately
       </Button>
     </>
