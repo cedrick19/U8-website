3dfb8d654cdb0c38c44a92a1ae24ee04eadaa080
sushikansai <michaelsagun013@gmail.com>
2025-04-23 15:12:56 +0800

    test fallback redirect

--- src/app.tsx
+++ src/app.tsx
@@ -27,7 +27,7 @@ const MyApp: React.FC = () => {
   const renderMobile = () => {
     return (
       <>
-        <View tab main url="/" browserHistory browserHistorySeparator="" animate={false}>
+        <View tab main url="/" browserHistory browserHistorySeparator="">
           <ToolMobile />
         </View>
       </>
