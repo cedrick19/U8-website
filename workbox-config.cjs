5c76138880f18257a81502c252df125425dabd3a
sushikansai <michaelsagun013@gmail.com>
2025-04-24 18:38:28 +0800

    adjusted workbox and package for offline caching

--- workbox-config.cjs
+++ workbox-config.cjs
@@ -1,7 +1,9 @@
 module.exports = {
   globDirectory: "www/",
-  globPatterns: ["**/*.{js,css,html,png,jpg,jpeg,svg,webp,avif,ico,woff,woff2}"],
+  globPatterns: ["**/*.{js,css,html,png,jpg,jpeg,svg,webp,avif,ico,woff,woff2,ttf}"],
   globIgnores: [],
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
   swDest: 'www/service-worker.js',
+  navigateFallback: '/index.html',
+  navigateFallbackDenylist: [/^\/_/, /\/[^\/]+.[^\/]+$/],
 };
