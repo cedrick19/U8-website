d9961bfbde37fe5952489336190256c6f8c11ce3
sushikansai <michaelsagun013@gmail.com>
2025-04-29 18:37:35 +0800

    update ProfilePage to include share

--- src/components/nav-bar/ToolMobile.tsx
+++ src/components/nav-bar/ToolMobile.tsx
@@ -1,5 +1,5 @@
 import { Block, f7, f7ready, Link } from 'framework7-react';
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { cn } from '@/utils/helper';
 import CoinActive from '@/assets/image/icons/coin-on.svg';
 import CoinInactive from '@/assets/image/icons/coin-off.svg';
@@ -42,41 +42,45 @@ export const ToolMobile = () => {
 
   return (
     <>
-      <Block className="absolute -bottom-9 z-10 flex h-24 w-full flex-row justify-between sm:h-40">
+      <Block className="absolute -bottom-9 z-10 flex h-24 w-full flex-row justify-between px-5 sm:h-40">
         <SvgMobile />
-        <Link
-          className="absolute bottom-10 left-1/2 h-14 w-14 -translate-x-1/2 rounded-full bg-primary-gradient shadow-xl shadow-[#613EEA]/50 sm:h-28 sm:w-28 md:bottom-16"
-          href="/recharge/"
-          onClick={() => handleNav('/recharge/')}
-        >
-          <img
-            src={activeUrl === '/recharge/' ? CoinActive : CoinInactive}
-            className="h-10 sm:h-20"
-          />
-        </Link>
+
         {MobileNavItems.map((item, ids) => {
           const tabUrl = item.tabLink === 'home' ? '/' : `/${item.tabLink}/`;
           return (
-            <Link
-              key={ids}
-              href={tabUrl}
-              rippleColor="none"
-              onClick={() => handleNav(tabUrl)}
-              className={cn('flex flex-col pt-2', ids % 2 === 0 ? 'pl-6' : 'pr-6')}
-            >
-              <img
-                src={activeUrl === tabUrl ? item.icons.iconOn : item.icons.iconOff}
-                className="h-6 sm:h-8 md:h-10"
-              />
-              <span
-                className={cn(
-                  'text-sm sm:text-2xl',
-                  activeUrl === tabUrl ? 'text-gradient' : 'text-inactive',
-                )}
+            <Fragment key={ids}>
+              <Link
+                key={ids}
+                href={tabUrl}
+                onClick={() => handleNav(tabUrl)}
+                className={cn('flex flex-col pt-2', ids % 2 === 0 ? 'pl-4' : 'pr-4')}
               >
-                {item.text}
-              </span>
-            </Link>
+                <img
+                  src={activeUrl === tabUrl ? item.icons.iconOn : item.icons.iconOff}
+                  className="h-6 sm:h-8 md:h-10"
+                />
+                <span
+                  className={cn(
+                    'text-sm sm:text-2xl',
+                    activeUrl === tabUrl ? 'text-gradient' : 'text-inactive',
+                  )}
+                >
+                  {item.text}
+                </span>
+              </Link>
+              {ids === 1 && (
+                <Link
+                  className="relative left-0.5 h-14 w-14 rounded-full bg-primary-gradient shadow-xl shadow-[#613EEA]/50 sm:h-28 sm:w-28 md:bottom-16"
+                  href="/recharge/"
+                  onClick={() => handleNav('/recharge/')}
+                >
+                  <img
+                    src={activeUrl === '/recharge/' ? CoinActive : CoinInactive}
+                    className="h-10 sm:h-20"
+                  />
+                </Link>
+              )}
+            </Fragment>
           );
         })}
       </Block>
