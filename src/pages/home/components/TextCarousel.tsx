1079c8465fcb516b4ad1494cc49ace61fa520636
cedy123 <cedrickgueta056@gmail.com>
2025-04-30 13:55:46 +0800

    "adjust: text carousel marquee"

--- src/pages/home/components/TextCarousel.tsx
+++ src/pages/home/components/TextCarousel.tsx
@@ -8,7 +8,7 @@ const marqueeData = [
   },
   {
     message: 'Congratulations to the player c**** in Philippines win',
-    game: 'Lotto',
+    game: 'Lotto.',
     price: '40000 Pesos',
   },
 ];
@@ -16,26 +16,22 @@ const marqueeData = [
 export const TextCarousel = () => {
   const renderMessages = () =>
     marqueeData.map(({ message, game, price }, index) => (
-      <span key={index} className="inline-flex items-center">
-        <span>{message} </span>
+      <span key={index} className="flex">
+        <span>{message}</span>
         <span className="mx-1 text-green-500">{price}</span>
         <span>of game</span>
         <span className="mx-1 text-blue-500">{game}</span>
-        {index !== marqueeData.length - 1}
       </span>
     ));
 
   return (
-    <div className="flex overflow-hidden rounded-full bg-primary-gradient p-1 md:container md:mx-auto">
+    <div className="flex rounded-full bg-primary-gradient p-1 md:container md:mx-auto">
       <div className="flex h-8 w-20 items-center justify-center">
         <img src={Announcement} className="h-6 w-6" alt="announcement" />
       </div>
-      <div className="relative flex w-full items-center overflow-hidden whitespace-nowrap rounded-r-full bg-white">
-        <div className="flex min-w-max animate-marquee whitespace-nowrap will-change-transform">
-          <div className="flex">
-            {renderMessages()}
-            {renderMessages()}
-          </div>
+      <div className="relative flex w-full items-center overflow-hidden rounded-r-full bg-white">
+        <div className="flex min-w-max animate-marquee will-change-transform">
+          {[...renderMessages(), ...renderMessages()]}
         </div>
       </div>
     </div>
