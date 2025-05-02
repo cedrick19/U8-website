0e8b88c2c7a01aadcce5dd7170396797789da9c8
cedy123 <cedrickgueta056@gmail.com>
2025-05-02 18:15:48 +0800

    "add: add types.ts and transfer types"

--- src/pages/profile/my-wallet/refresh/components/HuangwangPage.tsx
+++ src/pages/profile/my-wallet/refresh/components/HuangwangPage.tsx
@@ -3,19 +3,7 @@ import { Icon, Button, Block, Popup, List, ListItem, Page } from 'framework7-rea
 import { balanceSummary, countryCodes } from './utils';
 import CustomInput from '@/pages/profile/component/CustomInput';
 import { cn } from '@/utils/helper';
-
-type FormState = {
-  popupOpened: boolean;
-  selectedCode: string;
-  phoneNumber: string;
-  amount: string;
-};
-
-type CountryCode = {
-  id: number;
-  code: string;
-  country: string;
-};
+import { CountryCode, FormState } from './types';
 
 export const HuangWangPage = () => {
   const [formState, setFormState] = useState<FormState>({
