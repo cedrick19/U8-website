0c9b1b23cf6ee987d2ef8c3b0dc755fd6ba9cd9f
sushikansai <michaelsagun013@gmail.com>
2025-05-05 18:06:55 +0800

    deleted clickNavigate function

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -6,16 +6,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const clickNavigate = (id: string, url: string) => {
-  return {
-    id: id,
-    onClick: () => {
-      document.getElementById(id)?.blur();
-      f7.view.main.router.navigate(url);
-    },
-  };
-};
-
 export const f7navigate = (id: string, url: string): void => {
   document.getElementById(id)?.blur();
   f7.view.main.router.navigate(url);
