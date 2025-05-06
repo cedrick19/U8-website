f07436531bdcf88fe2727feb809db42f0c0aad90
virgil-ferolino <gitMaleFerolino@gmail.com>
2025-05-06 14:32:03 +0800

    fix: ProfilePage buttom spacing

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -105,127 +105,135 @@ const ProfilePage = () => {
           </>
         }
       />
-      <Block className="flex h-16 items-center">
-        <img
-          alt="Avatar"
-          slot="media"
-          src={AvatarIcon}
-          className="h-16 w-16 rounded-full bg-blue-500"
-        />
-        <Block className="flex-col gap-1">
-          <p className="text-lg">beluga.cat</p>
-          <div className="flex items-center">
-            <p className="text-xs">Member Account: {subCopy.subLabel}</p>
-            {subCopy.isCopied ? (
-              <Icon f7="checkmark" size={15} />
-            ) : (
-              <img src={Copy} onClick={handleCopy} className="h-4 w-4" />
-            )}
-          </div>
-        </Block>
-      </Block>
-      <ProfileCard
-        refresh
-        pCardTitle="My Wallet"
-        className="relative bg-[#ECE8F5] py-5 shadow-[#E1DCF3]"
-      >
-        <Card
-          className="z-10 border-2 border-white bg-primary-gradient shadow-md shadow-[#d4caf9]"
-          raised
-        >
-          <CardContent className="py-5">
-            <div className="flex w-full items-center justify-center gap-2">
-              <p className="text-md text-center text-white">Wallet Balance</p>
-              <Icon material="visibility_on" color="white" size={20} />
+      <Block className="px-0 pb-20">
+        <Block className="flex h-16 items-center">
+          <img
+            alt="Avatar"
+            slot="media"
+            src={AvatarIcon}
+            className="h-16 w-16 rounded-full bg-blue-500"
+          />
+          <Block className="flex-col gap-1">
+            <p className="text-lg">beluga.cat</p>
+            <div className="flex items-center">
+              <p className="text-xs">Member Account: {subCopy.subLabel}</p>
+              {subCopy.isCopied ? (
+                <Icon f7="checkmark" size={15} />
+              ) : (
+                <img src={Copy} onClick={handleCopy} className="h-4 w-4" />
+              )}
             </div>
-
-            <div
-              className="mx-auto flex w-fit items-center justify-center"
-              onClick={() => f7.view.main.router.navigate('my-wallet/')}
-            >
-              <p className="text-center text-2xl text-white">0.00</p>
-              <Icon material="chevron_right" color="white" size={30} />
-            </div>
-          </CardContent>
-        </Card>
-        <div className="flex items-center justify-center gap-5">
-          <Link className="flex gap-2 font-bold" href="/recharge/">
-            <img
-              src={CoinOff}
-              alt="recharge"
-              className="h-5 w-5 cursor-pointer rounded-full bg-primary-gradient p-[2.4px]"
-            />
-            <p>Recharge</p>
-          </Link>
-          <div className="h-8 border-[1px] border-slate-300" />
-          <Block className="flex font-bold">
-            <Link
-              className="flex items-center gap-2"
-              id="refresh"
-              onClick={() => f7navigate('refresh', 'refresh/')}
-            >
-              <div className="h-5 w-5 cursor-pointer rounded-full bg-primary-gradient p-1">
-                <img src={Refresh2} alt="refresh" className="h-full w-full" />
-              </div>
-              <p>Refresh</p>
-            </Link>
           </Block>
-        </div>
-      </ProfileCard>
+        </Block>
+        <ProfileCard
+          refresh
+          pCardTitle="My Wallet"
+          className="relative bg-[#ECE8F5] py-5 shadow-[#E1DCF3]"
+        >
+          <Card
+            className="z-10 border-2 border-white bg-primary-gradient shadow-md shadow-[#d4caf9]"
+            raised
+          >
+            <CardContent className="py-5">
+              <div className="flex w-full items-center justify-center gap-2">
+                <p className="text-md text-center text-white">Wallet Balance</p>
+                <Icon material="visibility_on" color="white" size={20} />
+              </div>
 
-      <ProfileCard
-        refresh
-        pCardTitle={
-          <p>
-            Today's Income: <span className="text-gradient">April 7</span>
-          </p>
-        }
-      >
-        <Card className="z-10 border-2 border-white bg-[#ECE8F5] shadow-md shadow-[#d4caf9]" raised>
-          <CardContent className="flex items-start justify-center gap-5 py-10">
-            {income.map(({ value, label, activeTab }, index) => (
+              <div
+                className="mx-auto flex w-fit items-center justify-center"
+                onClick={() => f7.view.main.router.navigate('my-wallet/')}
+              >
+                <p className="text-center text-2xl text-white">0.00</p>
+                <Icon material="chevron_right" color="white" size={30} />
+              </div>
+            </CardContent>
+          </Card>
+          <div className="flex items-center justify-center gap-5">
+            <Link className="flex gap-2 font-bold" href="/recharge/">
+              <img
+                src={CoinOff}
+                alt="recharge"
+                className="h-5 w-5 cursor-pointer rounded-full bg-primary-gradient p-[2.4px]"
+              />
+              <p>Recharge</p>
+            </Link>
+            <div className="h-8 border-[1px] border-slate-300" />
+            <Block className="flex font-bold">
+              <Link
+                className="flex items-center gap-2"
+                id="refresh"
+                onClick={() => f7navigate('refresh', 'refresh/')}
+              >
+                <div className="h-5 w-5 cursor-pointer rounded-full bg-primary-gradient p-1">
+                  <img src={Refresh2} alt="refresh" className="h-full w-full" />
+                </div>
+                <p>Refresh</p>
+              </Link>
+            </Block>
+          </div>
+        </ProfileCard>
+
+        <ProfileCard
+          refresh
+          pCardTitle={
+            <p>
+              Today's Income: <span className="text-gradient">April 7</span>
+            </p>
+          }
+        >
+          <Card
+            className="z-10 border-2 border-white bg-[#ECE8F5] shadow-md shadow-[#d4caf9]"
+            raised
+          >
+            <CardContent className="flex items-start justify-center gap-5 py-10">
+              {income.map(({ value, label, activeTab }, index) => (
+                <Fragment key={index}>
+                  <Link
+                    className="flex-col"
+                    id={`index-${index}`}
+                    onClick={() => handleNavigate(activeTab, index)}
+                  >
+                    <p className="text-center text-sm font-bold text-[#4A4A4A]">
+                      {value.toFixed(index === 0 ? 0 : 2)} <span>&gt;</span>
+                    </p>
+                    <p className="text-gradient text-center text-xs">{label}</p>
+                  </Link>
+                  {income.length - 1 !== index && (
+                    <div className="h-16 border-[1px] border-slate-300" />
+                  )}
+                </Fragment>
+              ))}
+            </CardContent>
+          </Card>
+        </ProfileCard>
+
+        <ProfileCard pCardTitle="More Services">
+          <Block className="grid grid-cols-3 gap-5 px-10 py-5">
+            {services.map((data, index) => (
               <Fragment key={index}>
                 <Link
-                  className="flex-col"
-                  id={`index-${index}`}
-                  onClick={() => handleNavigate(activeTab, index)}
+                  id={data.id}
+                  className="flex-col justify-items-center"
+                  onClick={() => f7navigate(`${data.id}`, `${data.id}/`)}
                 >
-                  <p className="text-center text-sm font-bold text-[#4A4A4A]">
-                    {value.toFixed(index === 0 ? 0 : 2)} <span>&gt;</span>
-                  </p>
-                  <p className="text-gradient text-center text-xs">{label}</p>
+                  <img alt={data.label} src={data.icon} className="h-10 w-10" />
+                  <p className="text-center">{data.label}</p>
                 </Link>
-                {income.length - 1 !== index && (
-                  <div className="h-16 border-[1px] border-slate-300" />
+                {index === 2 && (
+                  <Link
+                    onClick={() => setOpenShare(true)}
+                    className="flex-col justify-items-center"
+                  >
+                    <img alt="Share" src={Share} className="h-10 w-10" />
+                    <p className="text-center">Share</p>
+                  </Link>
                 )}
               </Fragment>
             ))}
-          </CardContent>
-        </Card>
-      </ProfileCard>
-
-      <ProfileCard pCardTitle="More Services" className="mb-24">
-        <Block className="grid grid-cols-3 gap-5 px-10 py-5">
-          {services.map((data, index) => (
-            <Fragment key={index}>
-              <Link
-                id={data.id}
-                className="flex-col justify-items-center"
-                onClick={() => f7navigate(`${data.id}`, `${data.id}/`)}
-              >
-                <img alt={data.label} src={data.icon} className="h-10 w-10" />
-                <p className="text-center">{data.label}</p>
-              </Link>
-              {index === 2 && (
-                <Link onClick={() => setOpenShare(true)} className="flex-col justify-items-center">
-                  <img alt="Share" src={Share} className="h-10 w-10" />
-                  <p className="text-center">Share</p>
-                </Link>
-              )}
-            </Fragment>
-          ))}
-        </Block>
-      </ProfileCard>
+          </Block>
+        </ProfileCard>
+      </Block>
 
       <CustomPopUp open={openShare} clAction={() => setOpenShare(false)} title="">
         <img src={ShareImg} className="w-full justify-self-center rounded-2xl" />
