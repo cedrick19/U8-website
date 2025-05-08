f2a0511e3e080f7d90ccfcce8a8a2bb6baf51897
sushikansai <michaelsagun013@gmail.com>
2025-05-08 18:42:08 +0800

    some adjustments in NotifComponent and HuiwangPage components

--- src/pages/notifications/components/NotifComponent.tsx
+++ src/pages/notifications/components/NotifComponent.tsx
@@ -1,7 +1,24 @@
 import { Block, Icon, Link } from 'framework7-react';
 import { NotificationDataType } from '@/pages/notifications/type';
+import { f7navigate } from '@/utils/helper';
 
 export const NotifComponent = ({ data }: { data: NotificationDataType[] }) => {
+  const formatting = (content: string) => {
+    return content.split('\n\n').map((para, ids) => (
+      <span key={ids}>
+        {para.split('\n').map((part, idk) =>
+          idk % 2 === 1 ? (
+            <span key={idk} className="font-bold">
+              {part}
+            </span>
+          ) : (
+            <span key={idk}>{part}</span>
+          ),
+        )}
+      </span>
+    ));
+  };
+
   return (
     <>
       {data.length !== 0 ? (
@@ -14,15 +31,15 @@ export const NotifComponent = ({ data }: { data: NotificationDataType[] }) => {
                   href={`/notifications/${notif.id}/`}
                   className="text-gradient flex flex-row items-center"
                   id={notif.id}
-                  onClick={() => {
-                    document.getElementById(notif.id.toString())?.blur();
-                  }}
+                  onClick={() => f7navigate(notif.id.toString(), `/notifications/${notif.id}/`)}
                 >
                   more
                   <Icon f7="chevron_right" icon="text-gradient" size={10} />
                 </Link>
               </div>
-              <p className="line-clamp-4 overflow-hidden text-ellipsis">{notif.content}</p>
+              <p className="line-clamp-4 overflow-hidden text-ellipsis">
+                {formatting(notif.content)}
+              </p>
               <p className="text-xs text-gray-500">
                 {notif.date} {notif.time}
               </p>
