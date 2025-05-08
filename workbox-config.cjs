ba13505651b66270aa899d1e005f9785302ce81b
sushikansai <michaelsagun013@gmail.com>
2025-05-08 18:28:24 +0800

    added sample service worker for development phase

--- workbox-config.cjs
+++ workbox-config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   globDirectory: "www/",
   globPatterns: ["**/*.{js,css,html,png,jpg,jpeg,svg,webp,avif,ico,woff,woff2,ttf}"],
-  globIgnores: [],
+  globIgnores: ['**/sampleSw.js', 'sampleSw.js', '**/sampleSw.*'],
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
   swDest: 'www/service-worker.js',
   navigateFallback: '/index.html',
