202ed4051108d1ebca55683dfa16ababa30e9449
cedy123 <cedrickgueta056@gmail.com>
2025-05-19 17:17:39 +0800

    refactor: homecarousel remove navigation

--- src/pages/home/components/HomeCarousel.tsx
+++ src/pages/home/components/HomeCarousel.tsx
@@ -1,61 +1,22 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Autoplay, Navigation } from 'swiper/modules';
-import { useState } from 'react';
-import { Button } from 'framework7-react';
-import IntroBanner from '@/assets/image/home/introbanner.jpg';
-import LinkBanner from '@/assets/image/home/linkbanner.jpg';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import { cn } from '@/utils/helper';
+import { Pagination, Autoplay } from 'swiper/modules';
+import IntroBanner from '@/assets/image/home/introBanner.jpg';
+import LinkBanner from '@/assets/image/home/linkBanner.jpg';
 
 export const HomeCarousel = () => {
-  const [hovered, setHovered] = useState(false);
   const carouselImages = [IntroBanner, LinkBanner];
-  const navButtonBaseClass =
-    'absolute top-1/2 hidden md:flex p-4  bg-opacity-60 transition-opacity ';
-
   return (
-    <div
-      className="relative"
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
+    <Swiper
+      pagination={{ clickable: true }}
+      autoplay={{ delay: 4000 }}
+      modules={[Pagination, Autoplay]}
     >
-      <Swiper
-        slidesPerView={1}
-        pagination={{ clickable: true }}
-        autoplay={{ delay: 4000, disableOnInteraction: false }}
-        navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        }}
-        modules={[Navigation, Pagination, Autoplay]}
-        className="w-full sm:h-10 md:h-auto"
-      >
-        {carouselImages.map((image, index) => (
-          <SwiperSlide key={index}>
-            <img
-              src={image}
-              alt={`Promotional Banner ${index + 1}`}
-              className="h-auto w-full sm:h-full"
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-
-      <div className="hidden md:block">
-        {['prev', 'next'].map((dir) => (
-          <Button
-            key={dir}
-            className={cn(
-              `swiper-button-${dir}`,
-              dir === 'prev' ? 'left-4' : 'right-4',
-              hovered ? 'opacity-100' : 'opacity-0',
-              navButtonBaseClass,
-            )}
-          />
-        ))}
-      </div>
-    </div>
+      {carouselImages.map((image, index) => (
+        <SwiperSlide key={index}>
+          <img src={image} alt={`Promotional Banner ${index + 1}`} className="w-full" />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
